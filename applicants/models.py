c12deaa18a91e6bbe25eb44d3c5c716859847697
Lennox Mountain <lennoxmountain67@gmail.com>
2025-03-11 21:07:12 +0200

    separate subject and grade models

--- applicants/models.py
+++ applicants/models.py
@@ -5,16 +5,18 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Subject(models.Model):
-    name = models.CharField(max_length=100)
-    grade = models.IntegerField(validators=[
-        MinValueValidator(1),
-        MaxValueValidator(9)
+    name = models.CharField(max_length=100, unique=True, blank=False)
+
+class Grade(models.Model):
+    subject = models.ManyToManyField(Subject)
+    score = models.IntegerField(validators=[
+        MinValueValidator(1), MaxValueValidator(9)
     ])
 class Applicant(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     school = models.CharField(max_length=200)
-    subjects = models.ManyToManyField(Subject)
+    subjects = models.ManyToManyField(Grade)
     program = models.ManyToManyField(Program)
 
     def __str__(self):
--- programs/models.py
+++ programs/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+class Requirement(models.Model):
+    subject = models.CharField(unique=True, blank=False, max_length=100)
+    restriction = 
 class Program(models.Model):
     course_code = models.CharField(max_length=10)
     name = models.CharField(max_length=255)
