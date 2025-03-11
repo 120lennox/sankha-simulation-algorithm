ec8855027ce5347a31734b90b4e1418db49ea796
Lennox Mountain <lennoxmountain67@gmail.com>
2025-03-11 22:31:53 +0200

    modified subjects models

--- programs/admin.py
+++ programs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Program
+from .models import Program, Requirement
 
 # Register your models here.
+admin.site.register(Requirement)
 admin.site.register(Program)
--- subjects/models.py
+++ subjects/models.py
@@ -5,8 +5,16 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Subject(models.Model):
     name = models.CharField(max_length=100, unique=True, blank=False)
 
+    def __str__(self):
+        return self.name
+
 class Grade(models.Model):
-    subject = models.ManyToManyField(Subject)
+    subject = models.ForeignKey(Subject, null=True, on_delete=models.CASCADE)
     score = models.IntegerField(validators=[
         MinValueValidator(1), MaxValueValidator(9)
-    ])
+    ], 
+    null= True
+    )
+
+    def __str__(self):
+        return f"{self.subject} (grade: {self.score})"
