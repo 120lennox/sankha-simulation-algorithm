886bc247edae8d07fc0fe2434d4eb78903ffeaf3
Lennox Mountain <lennoxmountain67@gmail.com>
2025-03-11 14:36:15 +0200

    added applicants

--- applicants/models.py
+++ applicants/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    grade = models.IntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(9)
+    ])
+class Applicants(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    school = models.CharField(max_length=200)
+    subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.name
+
