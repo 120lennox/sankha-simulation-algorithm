ff72f37c3a81ea7b17de51e9afe69cfa4e1ef4eb
Lennox Mountain <lennoxmountain67@gmail.com>
2025-03-11 15:58:19 +0200

    added programs section

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class Program(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    requirements = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+ 
