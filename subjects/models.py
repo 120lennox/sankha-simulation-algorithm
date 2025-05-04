6234c7d7b0dbb81b2eb891f4880adbeb084a9389
Lennox Mountain <lennoxmountain67@gmail.com>
2025-05-04 15:25:15 +0200

    new day, new code, new updates

--- subjects/models.py
+++ subjects/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # models
 class Subject(models.Model):
-    name = models.CharField(max_length=100, unique=True, blank=False)
+    name = models.CharField(max_length=100, unique=True, blank=False, default="English")
 
     def __str__(self):
         return self.name
