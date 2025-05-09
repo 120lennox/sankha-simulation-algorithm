3a388dae3f2290a527864a335be55c29c87e424d
Lennox Mountain <lennoxmountain67@gmail.com>
2025-05-09 20:04:16 +0200

    started sankha engine

--- sankha_engine/db_interfaces.py
+++ sankha_engine/db_interfaces.py
@@ -1,5 +1,7 @@
 from programs.models import Program
 from applicants.models import Applicant
+from subjects.models import Subject
+
 class DBInterface:
     def __init__(self):
         pass
@@ -24,3 +26,30 @@ class DBInterface:
             return programs
         except:
             return None
+    
+    def get_all_applicants(self):
+        try:
+            applicants = Applicant.objects.all()
+            return applicants
+        except:
+            return None
+    
+    def get_subjects(self, subject_id):
+        try:
+            subject = Subject.objects.all(id=subject_id)
+            return subject
+        except:
+            return None
+    
+    def get_all_subjects(self, subject_id):
+        try:
+            subjects = Subject.objects.all()
+            return subjects
+        except:
+            return None
+    
+    def get_student(self, student_id):
+        pass
+
+    def get_all_students(self):
+        pass
--- sankha_engine/sankha.py
+++ sankha_engine/sankha.py
@@ -1,2 +1,12 @@
-class Sankha:
-    pass
+class SankhaEngine:
+    def __init__(self):
+        pass
+
+    def calculate_aggregate_score(self):
+        pass
+
+    def select_students(self):
+        pass
+
+    def update_selected_students(self):
+        pass
